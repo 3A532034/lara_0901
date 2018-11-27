1a6393f8a2e587d34ad214eec9dd7eeae6d0072d
吳駿松 <jo32229hn@gmail.com>
2018-11-27 23:13:20 +0800

    在 PostsController的 destroy內刪除資料

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -31,6 +31,12 @@ class PostsController extends Controller
         $post->update($request->all());
         return redirect()->route('admin.posts.index');
     }
+    public function destroy($id)
+    {
+        Post::destroy($id);
+        returnredirect()->route('admin.posts.index');
+    }
+
 
 
 }
